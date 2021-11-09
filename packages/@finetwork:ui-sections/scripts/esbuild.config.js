a19faefc7ac3c9a18567bae21aea6f34383f29f2
Fran García <fran17901@gmail.com>
2021-11-09 09:46:53 +0100

    Feature/esbuild (#33)
    * Switch to esbuild to build tooling
    
    * Change tsconfig and options to get source files in develop

---
+++ packages/@finetwork:ui-sections/scripts/esbuild.config.js
@@ -0,0 +1,16 @@
+const esbuild = require('esbuild')
+
+const { nodeExternalsPlugin } = require('esbuild-node-externals')
+
+esbuild
+  .build({
+    entryPoints: ['./src/index.ts'],
+    outfile: 'dist/index.js',
+    bundle: true,
+    minify: true,
+    platform: 'node',
+    sourcemap: true,
+    target: 'node14',
+    plugins: [nodeExternalsPlugin()],
+  })
+  .catch(() => process.exit(1))
--- packages/@finetwork:ui/scripts/build.sh
+++ packages/@finetwork:ui/scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -rf ./dist
-echo "Building library..."
-./node_modules/.bin/rollup -c
+./node_modules/.bin/rimraf dist
+echo "Building library and generating types..."
+./node_modules/.bin/npm-run-all build-types build-lib
+echo "Copying README..."
 cp ../../README.md ./dist/README.md
+echo "Copying package.json..."
+cp ./package.json ./dist/package.json
 echo "Library built in dist folder!"
