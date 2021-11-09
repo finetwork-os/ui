9c4cd0d50a5a0878679550ed85cdfac3272485d3
frgarciames <fran17901@gmail.com>
2021-11-09 09:45:46 +0100

    Change tsconfig and options to get source files in develop

--- packages/@finetwork:ui/scripts/build.sh
+++ packages/@finetwork:ui/scripts/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 ./node_modules/.bin/rimraf dist
-echo "Building library and generating types"
+echo "Building library and generating types..."
 ./node_modules/.bin/npm-run-all build-types build-lib
+echo "Copying README..."
 cp ../../README.md ./dist/README.md
+echo "Copying package.json..."
+cp ./package.json ./dist/package.json
 echo "Library built in dist folder!"
