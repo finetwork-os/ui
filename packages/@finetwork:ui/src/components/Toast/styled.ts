6c4cb77a8f3db13350a2f1a4a4a6bed140f66510
Santiago Molto <santiagomoltomartinez@gmail.com>
2022-03-21 12:40:59 +0100

    Removed default color from boxShadow because it doesnt change anymore

--- packages/@finetwork:ui/src/components/Toast/styled.ts
+++ packages/@finetwork:ui/src/components/Toast/styled.ts
@@ -119,7 +119,7 @@ export const StyledToast = styled(ToastPrimitive.Root, {
   gridTemplateColumns: 'auto max-content',
   alignItems: 'center',
   boxShadow:
-    '$colors$default 0px 10px 38px -10px, $colors$default 0px 10px 20px -15px',
+    'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
   '&:hover': {
     cursor: 'grab',
   },
