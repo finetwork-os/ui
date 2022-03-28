f2819c4adc751f9963cfa5de4307f7dd25d1ee39
Santiago Molto <santiagomoltomartinez@gmail.com>
2022-03-28 16:23:56 +0200

    Font Druk added to storybook

--- stories/FiButton.stories.tsx
+++ stories/FiButton.stories.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 
 import { Button } from '@finetwork/ui'
+import { ThemeProvider } from '@finetwork/ui'
 
+import { theme } from './theme'
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/FiButton',
@@ -14,28 +16,8 @@ export default {
 } as ComponentMeta<typeof Button>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
-
-export const Primary = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-}
-
-export const Secondary = Template.bind({})
-Secondary.args = {
-  label: 'Button',
-}
-
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-  label: 'Button',
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
-  label: 'Button',
-}
+export const Template: ComponentStory<typeof Button> = (args) => (
+  <ThemeProvider theme={theme}>
+    <Button {...args}>Hola</Button>
+  </ThemeProvider>
+)
