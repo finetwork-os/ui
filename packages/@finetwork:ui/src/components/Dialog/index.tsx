53afaec8ab25252f9c79180dac948444d6dcb363
Sergio Fernandez <sergiofernandezjimenez1997@outlook.es>
2023-05-09 15:40:38 +0200

    Fix: Not go up when dialog is open

--- packages/@finetwork:ui/src/components/Dialog/index.tsx
+++ packages/@finetwork:ui/src/components/Dialog/index.tsx
@@ -80,25 +80,36 @@ export const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(
       }
 
       if (!bottomSheet && width !== 'full') {
-        dialogElement.style.animation = `${isOpen ? dialogAnimationOpen : dialogAnimationClose} 0.4s cubic-bezier(0.69,-0.37,0.24,1.48) forwards`
+        dialogElement.style.animation = `${
+          isOpen ? dialogAnimationOpen : dialogAnimationClose
+        } 0.4s cubic-bezier(0.69,-0.37,0.24,1.48) forwards`
       }
 
       if (bottomSheet) {
-        dialogElement.style.animation = `${isOpen ? `${animationSelectMobile} cubic-bezier(0.72,-0.67,0.49,0.01)` : `${animationCloseSelectMobile} linear`} 0.25s forwards`
+        dialogElement.style.animation = `${
+          isOpen
+            ? `${animationSelectMobile} cubic-bezier(0.72,-0.67,0.49,0.01)`
+            : `${animationCloseSelectMobile} linear`
+        } 0.25s forwards`
       }
 
       if (width === 'full') {
-        dialogElement.style.animation = `${isOpen ? fullDialogAnimationOpen : fullDialogAnimationClose} 0.4s ease forwards`
+        dialogElement.style.animation = `${
+          isOpen ? fullDialogAnimationOpen : fullDialogAnimationClose
+        } 0.4s ease forwards`
       }
 
       if (overlay) {
-        overlayElement.style.animation = `${isOpen ? fadeInBackground : fadeOutBackground} 0.4s forwards`
+        overlayElement.style.animation = `${
+          isOpen ? fadeInBackground : fadeOutBackground
+        } 0.4s forwards`
       }
 
-      if (!isOpen) setTimeout(() => {
-        dialogElement.style.display = 'none'
-        overlayElement.style.display = 'none'
-      }, 400)
+      if (!isOpen)
+        setTimeout(() => {
+          dialogElement.style.display = 'none'
+          overlayElement.style.display = 'none'
+        }, 400)
     }
 
     React.useEffect(() => {
@@ -109,18 +120,26 @@ export const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(
       setCustomStyle(handleDialogCssProps(borderRadius, closeButtonSize, width))
     }, [borderRadius, closeButtonSize, width])
 
+    function disableScroll() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+      const scrollLeft =
+        window.pageXOffset || document.documentElement.scrollLeft
+      window.onscroll = function () {
+        window.scrollTo(scrollLeft, scrollTop)
+      }
+    }
+
+    function enableScroll() {
+      window.onscroll = function () {}
+    }
+
     React.useEffect(() => {
-      document.body.style.cssText = isOpen
-        ? `
-          position: fixed;
-          inline-size: 100%;
-          overflow-y: scroll;
-        `
-        : ''
       if (isOpen) {
+        disableScroll()
         document.addEventListener('click', handleOutsideClick, true)
         document.addEventListener('keydown', handleKeyPress, true)
       } else {
+        enableScroll()
         document.removeEventListener('click', handleOutsideClick, true)
         document.removeEventListener('keydown', handleKeyPress, true)
       }
